204d3604d805c9c77fc98a5c7bca3e6aacace571
Do Duy Anh <duyanhvn1999@gmail.com>
2022-06-27 11:17:40 +0700

    Initialize project using Create React App

---
+++ src/constants/helmet.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_HELMET_META = {
+  TITLE: 'React Codebase',
+  DESCRIPTION: 'This is basic react codebase',
+  IMAGE: 'https://github.githubassets.com/images/modules/logos_page/Octocat.png',
+  URL: 'https://github.com/',
+  TYPE: 'website',
+};
